a539c149606b9d19a86f14366cbd952c96da901a
Endris M <EndrisMd@gmail.com>
2023-05-04 19:41:31 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,7 +6,10 @@
 
 def minOperations(n):
     '''Ccalculates the fewest number of operations needed to result
-    in exactly n H characters.
+       in exactly n H characters.
+       Prototype: def minOperations(n)
+       Returns an integer
+       If n is impossible to achieve, return 0
     '''
     if not isinstance(n, int):
         return 0
